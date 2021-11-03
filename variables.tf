2a3f1f71f5f48b4d8fba7df1a893eb4deb6fe0a8
Christopher Pateman <christopher.pateman@version1.com>
2021-11-03 16:22:26 +0000

    remove validation

--- variables.tf
+++ variables.tf
@@ -35,10 +35,6 @@ variable "api_service_url" {
   type        = string
   description = "APIM API Service URL"
   default     = ""
-  validation {
-    condition     = var.api_service_url == "" || regex("(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})", var.api_service_url)
-    error_message = "Service URL should be a valid URL."
-  }
 }
 variable "api_subscription_required" {
   type        = bool
@@ -69,10 +65,6 @@ variable "policy_xml_link" {
   type        = string
   description = "APIM API Policy Link"
   default     = ""
-  validation {
-    condition     = var.policy_xml_link == "" || regex("(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})", var.policy_xml_link)
-    error_message = "XML Link should be provided as a valid URL."
-  }
 }
 
 
