a86367119e24780eb349aefd31dc3898acca3456
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-07-26 10:57:07 +0100

    revert changes

--- version.tf
+++ version.tf
@@ -1,13 +1,3 @@
 terraform {
-  required_version = ">= 1.3.7"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.42.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
+  required_version = ">= 1.0.4"
 }
